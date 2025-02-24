f530c1798a249d1a9ed6c4e2f0a2e999ec28e629
ConnectiveTCS <kyle@acewebdesign.co.za>
2025-02-24 11:48:37 +0400

    Enhance hero section and footer layout for improved responsiveness and styling

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,26 +1,33 @@
-<section class="h-[80vh] bg-black flex flex-col justify-center items-center"
+<section class="h-[80vh] bg-black flex flex-col justify-center items-center px-4"
     style="background-image: url({{ $bg }}); background-size: cover; background-position: center;">
     {{-- Overlay --}}
     <div class="w-screen h-[80vh] z-10 heroBG absolute"></div>
     {{-- Center Content --}}
     <div
         class="text-black text-center z-20 relative flex flex-col justify-center items-center rounded-md w-full sm:max-w-[50%] text-balance py-2 px-4 sm:px-10 bg-white bg-opacity-80 backdrop-blur shadow-xl">
-        <img src="{{ asset('assets/150Badge.png') }}" alt="" class="w-auto min-h-20 max-h-[150px] {{ $hideImg ?? 'display:block' }}">
-        <div class="{{ $hidden  ?? 'flex' }} flex-row items-center gap-2 " style=" "><hr  style="border: 2px solid red; width: 50px"><p>{{ $topPHeading ?? 'Become a Part of Our Legacy' }}</p><hr style="border: 2px solid red; width: 50px"></p></div>
-        <h1 class="montagu-slab-h1 text-center text-4xl md:text-5xl">
+        <img src="{{ asset('assets/150Badge.png') }}" alt=""
+            class="md:min-h-20 h-[100px] md:max-h-[150px] {{ $hideImg ?? 'display:block' }}">
+        <div class="{{ $hidden ?? 'flex' }} flex-row items-center gap-2 " style=" ">
+            <hr style="border: 2px solid red; width: 50px">
+            <p>{{ $topPHeading ?? 'Become a Part of Our Legacy' }}</p>
+            <hr style="border: 2px solid red; width: 50px">
+            </p>
+        </div>
+        <h1 class="montagu-slab-h1 my-4 text-center text-2xl md:text-5xl text-pretty">
             {{ $h1 ?? 'Reuniting Generations, Rekindling Memories.' }}
         </h1>
-        <p class="text-center text-2xl md:text-xl text-pretty w-[82%]">
+        <p class="text-center mb-4 text-lg md:text-xl text-pretty md:w-[82%] text-balance">
             {{ $p ?? 'Join us for a year-long celebration of our 150th anniversary, featuring events, reunions, and' }}
         </p>
-        <div class="flex flex-row gap-1 items-center justify-center">
-            @if(isset($button1))
+        <div class="flex flex-row gap-1">
+            @if (isset($button1))
                 @include($button1['component'], $button1)
             @endif
-            @if(isset($button2))
+            @if (isset($button2))
                 @include($button2['component'], $button2)
             @endif
-            <button data-modal-target="default-modal" data-modal-toggle="default-modal" type="button" class="min-h-[40px] block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Donate</button>
+            <button data-popover-target="popover-default" type="button"
+                class="py-2 px-4 bg-[{{ $bgColor ?? '#DE2413' }}] text-white hover:bg-white hover:text-[#DE2413] transition-all mt-{{ $mt ?? 1 }} {{ $hiddenBTN ?? 'hidden' }}">Donate</button>
         </div>
     </div>
 </section>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,23 @@
 <footer>
     <div class="relative">
         {{-- Centered Badge Image --}}
-        <div class="absolute w-full flex justify-center" style="top: 45%;">
+        <div class="absolute w-full justify-center md:top-[45%] top-[35%] hidden md:flex">
             <img src="{{ asset('assets/GHS_Badge.png') }}" alt="GHS Badge" class="h-12 w-12 md:h-auto md:w-20 z-20">
         </div>
-        
+
         {{-- Main Grid --}}
-        <div class="h-[70vh] grid grid-cols-2 grid-rows-2 gap-0">
-            <a href="/club_150" class="bg-[#DE2413] hover:bg-[#de2413d3] montagu-slab-h1 text-white flex justify-center items-center text-3xl">Club 150</a>
-            <a href="/programme" class="bg-[#262A40] hover:bg-[#262a40e6] montagu-slab-h1 text-white flex justify-center items-center text-3xl">Programme</a>
-            <a href="/pillar_project" class="bg-[#262A40] hover:bg-[#262a40e6] montagu-slab-h1 text-white flex justify-center items-center text-3xl">Pillar Project</dav>
-            <a href="/alumni" class="bg-[#DE2413] hover:bg-[#de2413d3] montagu-slab-h1 text-white flex justify-center items-center text-3xl">Alumni</a>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-0 h-auto md:h-[70vh]">
+            <a href="/club_150"
+                class="bg-[#DE2413] hover:bg-[#de2413d3] montagu-slab-h1 text-white flex justify-center items-center text-xl py-4 md:text-3xl">Club
+                150</a>
+            <a href="/programme"
+                class="bg-[#262A40] hover:bg-[#262a40e6] montagu-slab-h1 text-white flex justify-center items-center text-xl py-4 md:text-3xl">Programme</a>
+                <hr class="md:hidden block border-[#DE2413] w-full">
+            <a href="/pillar_project"
+                class="bg-[#262A40] hover:bg-[#262a40e6] montagu-slab-h1 text-white flex justify-center items-center text-xl py-4 md:text-3xl">Pillar
+                Project</a>
+            <a href="/alumni"
+                class="bg-[#DE2413] hover:bg-[#de2413d3] montagu-slab-h1 text-white flex justify-center items-center text-xl py-4 md:text-3xl">Alumni</a>
         </div>
     </div>
 </footer>
