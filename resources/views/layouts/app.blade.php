7665ba2ac07d71f78d672c36885dd607fe6f6834
ConnectiveTCS <kyle@acewebdesign.co.za>
2025-02-25 13:22:30 +0400

    Add head layout for font preloading and external JS loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
     {{-- Flowbite Integration --}}
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 
+    @include('layouts.head')
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,17 @@
+<!-- Preload non-render-blocking font CSS -->
+<link rel="preload" href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" as="style"
+    onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+    <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap">
+</noscript>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=YourFontFamily&display=swap" as="style"
+    onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=YourFontFamily&display=swap">
+</noscript>
+
+<!-- Load external JS with defer (non-critical) -->
+<script src="https://cdn.jsdelivr.net/npm/flowbite@1.6.6/dist/flowbite.min.js" defer></script>
