62a1b2d1439fa1e4bfeb01df08e2d3b27e2b495d
ConnectiveTCS <kyle@acewebdesign.co.za>
2025-02-25 10:30:54 +0400

    Adjust logo image dimensions for better responsiveness in guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
             <div>
                 <a href="/">
                     {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
-                    <img src="{{ asset('assets/GHS_Badge.png') }}" alt="Logo" class="w-20 h-20 fill-current text-gray-500">
+                    <img src="{{ asset('assets/GHS_Badge.png') }}" alt="Logo" class="w-auto h-20 fill-current text-gray-500">
                 </a>
             </div>
 
